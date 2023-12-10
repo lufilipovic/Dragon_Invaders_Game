4c98f03b1cad1223da2ade0daa4e2a3d0e52e045
Lucija Filipovic <lucija_filipovic_@hotmail.com>
2023-12-10 19:37:16 +0200

    Adding second level

--- Assets/Scripts/Enemy/EnemyCount.cs
+++ Assets/Scripts/Enemy/EnemyCount.cs
@@ -14,6 +14,7 @@ public class EnemyCount : MonoBehaviour
     public TMP_Text winningText;
 
     public Button restartButton;
+    public Button continueButton;
     public Button exitButton;
 
     public GameObject gameOverPanel;
@@ -21,17 +22,22 @@ public class EnemyCount : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //remove objects at start
         gameOverPanel.gameObject.SetActive(false);
         winningText.gameObject.SetActive(false);
         restartButton.gameObject.SetActive(false);
+        continueButton.gameObject.SetActive(false);
         exitButton.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //setting text to number of enemies
         enemyLeftText.text = "Enemies Left: " + EnemyBehaviour.enemiesLeft.ToString();
         enemyKilledText.text = "Enemies Killed: " + EnemyBehaviour.enemiesKilled.ToString();
+
+        //check if the player killed all enemies
         if (EnemyBehaviour.enemiesKilled > 0 && EnemyBehaviour.enemiesLeft == 0)
         {
             winGame();
@@ -39,21 +45,38 @@ public class EnemyCount : MonoBehaviour
 
     }
 
+    //show objects on call of the method
     public void winGame()
     {
         gameOverPanel.gameObject.SetActive(true);
         winningText.gameObject.SetActive(true);
         restartButton.gameObject.SetActive(true);
+        continueButton.gameObject.SetActive(true);
         exitButton.gameObject.SetActive(true);
 
     }
 
+    //reload the current scene
     public void restartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
+    //load the next scene from the build
+    public void continueGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //load first scene from the build
     public void startScreen()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+    //load the previous scene from the build
+    public void goBackScreen()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
